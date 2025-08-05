651229ce110979e49cc04bcc5778af34d754408d
Muhammad Aman Khan <muhammadamankhan182@gmail.com>
2025-08-05 23:05:22 +0500

    My  update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,14 @@ const Header = () => {
          style={{backgroundImage: "url('/header_img.png')"}} 
          id="Header">
     <Navbar/>
+    <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+      <h2 className="font-outfit text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center max-w-2xl mx-auto px-4">
+        Explore homes that fit your dreams</h2>
+      <div className="space-x-6 mt-16">
+          <a href="#Project" className="border border-white px-8 py-3 rounded">Projects</a>
+          <a href="#Contact us" className="bg-blue-500 px-8 py-3 rounded">Contact Us</a>
+      </div>
+     </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { assets } from '../assets/assets';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [showMobilemenu, setShowMobileMenu] = useState(false);
+
   return (
+    <>
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center px-6 py-4 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="Logo" className="h-10" />
@@ -16,8 +20,24 @@ const Navbar = () => {
         <button className="hidden md:block bg-white text-black px-8 py-2 rounded-full cursor-pointer hover:bg-gray-400 transition">
           Sign Up
         </button>
+        <img onClick={()=> setShowMobileMenu(true)} src={assets.menu_icon}className='md:hidden w-7 cursor-pointer' alt="" />
+      </div>
+
+      {/* {-----------mobile menu -----------} */}
+
+      <div className={`md:hidden ${showMobilemenu ?  'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+        <div className='flex justify-end p-6 cursor-pointer'>
+          <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className='w-6' alt="" />
+        </div>
+        <ul className='flex flex-col items-center mt-5 px-5 gap-2 text-lg font-medium '>
+          <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+          <a href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+          <a href="#Project" className='px-4 py-2 rounded-full inline-block'>Project</a>
+          <a href="#Testimonials" className='px-4 py2 rounded-full inline-block'>Testimonials</a>
+        </ul>
       </div>
     </div>
+    </>
   );
 };
 
